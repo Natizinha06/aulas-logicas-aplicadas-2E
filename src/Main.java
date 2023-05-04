f356d8c986f4d4e90183ab8b1e41fb04740c88a9
Natizinha06 <nathalybertolino2411@gmail.com>
2023-05-04 11:55:28 -0300

    Aula de introdução 00

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-       //laço enquanto - while
-        int contador = 0;
-        while(contador < 18); {
-            System.out.println("contador =" + contador);
-             //incremento
-            contador = contador + 1;
-        }
-        // laço do para faça - for
-        for(int x = 0; x < 10; x++){
-            System.out.println("contador =" + x);
-        }
-        // laço faça enquanto- do while
-        int cont=0;
-        do{
-            // processo
-            System.out.println(cont);
-            cont++; //incremento
-            }while(cont <= 10);
+        //Aula introdutória de Orientação à objetos
     }
 }
