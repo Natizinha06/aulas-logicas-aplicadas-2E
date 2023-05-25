cf7168b50ab1b04a1de38696cac555d1f0acc172
Natizinha06 <nathalybertolino2411@gmail.com>
2023-05-25 12:14:15 -0300

    Aula do dia 25/05/2023

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,23 @@ public class Main {
     public static void main(String[] args) {
         //Aula introdutória de Orientação à objetos
         Pessoa adao = new Pessoa();
+        //Declaração de objeto
+        Pessoa qualquer;
+        //instaciação do objeto
+        qualquer = new Pessoa();
+        // Deinição de formato da pessoa qualquer
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        // Criar novos objetos
+        Pessoa rainha;
+        //istanciar objetos
+        rainha = new Pessoa();
+        // definir forma do objeto
+        rainha.nome = "tina";
+        rainha.sobrenome = "Tunner";
+        // Definir comportamento do objeto
+        rainha.falar();
+        System.out.println(rainha.falar("alto"));
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,4 +2,10 @@ public class Pessoa {
     // Atributos da pessoa
     public String nome;
     public String sobrenome;
+    // Metodo de classe
+    public void falar() { System.out.println("falei");}
+    //sobrecarga do metodo falat
+    public String falar(String volume){
+        return "falei" + volume;
+    }
 }
